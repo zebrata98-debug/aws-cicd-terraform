6a933a1d69e715923c42d6444f2d5ba3708936a2
stan-debug <zebrata98@gmail.com>
2026-03-07 18:13:49 +0200

    Add ECR repository configuration in ecr.tf

---
+++ terraform/ecr.tf
@@ -0,0 +1,12 @@
+resource "aws_ecr_repository" "app" {
+  name                 = var.app_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
