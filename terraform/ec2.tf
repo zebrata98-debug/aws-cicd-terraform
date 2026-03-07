c0edf5eda3f895b27233e4b3a2511f9b3e9c4309
stan-debug <zebrata98@gmail.com>
2026-03-07 18:14:06 +0200

    Add EC2 instance and security group configuration
    This Terraform configuration sets up an EC2 instance with a security group allowing HTTP and SSH access. It also includes an output for the public IP of the instance.

---
+++ terraform/ec2.tf
@@ -0,0 +1,56 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  name = "${var.app_name}-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install docker -y
+    service docker start
+    usermod -a -G docker ec2-user
+    yum install -y aws-cli
+  EOF
+
+  tags = {
+    Name = var.app_name
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
